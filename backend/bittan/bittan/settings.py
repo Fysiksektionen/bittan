f317dd192e034d71b932632069e7e478ae310775
Gabriel <gabriel.m.calota@gmail.com>
2023-12-14 21:37:29 +0100

    Fix Django configuration to use correct porstgres.

--- backend/bittan/bittan/settings.py
+++ backend/bittan/bittan/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'bittan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgres',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bittan_db',
 
         'USER': 'postgres',  
