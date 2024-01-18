04cc965a568990e8cc648b4e5032fdf840e1d126
Leo Trolin <leo.trolin@gmail.com>
2024-01-18 16:19:15 +0100

    Add all fields to model ChapterEvent

--- backend/bittan/bittan/models.py
+++ backend/bittan/bittan/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 import datetime
 
-class ChapterEvent(models.Model):
-	title = models.TextField()
-
 class TicketType(models.Model):
 	price = MoneyField(max_digits=19, decimal_places=2, default_currency='SEK')
 	title = models.TextField()
 	description = models.TextField()
 	reservation_duration = models.DurationField(default=datetime.timedelta(hours=1))
+
+class ChapterEvent(models.Model):
+	title = models.TextField()
+	description = models.TextField()
+	max_tickets = models.IntegerField()
+	sales_stop_at = models.DateTimeField()
+	ticket_types = models.ManyToManyField(TicketType)
--- backend/bittan/scripts/create_mockdata.py
+++ backend/bittan/scripts/create_mockdata.py
@@ -9,10 +9,14 @@ django.setup()
 
 from django.contrib.auth.models import User
 from bittan.models import TicketType, ChapterEvent
+import datetime
 
 User.objects.create_superuser("admin", None, "admin")
 
-TicketType(price=199.99, title="Standardbiljett", description="En vanlig biljett.").save()
-TicketType(price=99.50, title="Studentbiljett", description="En billigare biljett.").save()
+NOW = datetime.datetime.now()
 
-ChapterEvent(title="Fysikalen Dag 1").save()
+standardbiljett = TicketType.objects.create(price=199.99, title="Standardbiljett", description="En vanlig biljett.")
+studentbiljett = TicketType.objects.create(price=99.50, title="Studentbiljett", description="En billigare biljett.")
+
+chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365))
+chapter_event1.ticket_types.add(standardbiljett)
