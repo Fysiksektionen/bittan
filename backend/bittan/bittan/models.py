4ddf145714ac3a8803c4dd38a2f6273aa5c862c3
Leo Trolin <leo.trolin@gmail.com>
2024-01-25 19:14:25 +0100

    Create models Payment and Ticket

--- backend/bittan/bittan/models.py
+++ backend/bittan/bittan/models.py
@@ -14,3 +14,28 @@ class ChapterEvent(models.Model):
 	max_tickets = models.IntegerField()
 	sales_stop_at = models.DateTimeField()
 	ticket_types = models.ManyToManyField(TicketType)
+
+class PaymentStatus(models.TextChoices):
+	ALIVE = "ALIVE"
+	FAILED_EXPIRED_RESERVATION = "FAILED_EXPIRED_RESERVATION"
+	FAILED_BY_ADMIN = "FAILED_BY_ADMIN"
+
+class Payment(models.Model):
+	time_created = models.DateTimeField()
+	swish_id = models.TextField()
+	status = models.TextField(choices=PaymentStatus)
+	telephone_number = models.TextField()
+	email = models.TextField()
+	total_price = MoneyField(max_digits=19, decimal_places=2, default_currency='SEK')
+	sent_email = models.BooleanField(default=False)
+
+class TicketStatus(models.TextChoices):
+	PAID = "PAID"
+	FAILED_BY_ADMIN = "FAILED_BY_ADMIN"
+
+class Ticket(models.Model):
+	external_id = models.TextField()
+	time_created = models.DateTimeField()
+	payment = models.ForeignKey(Payment, on_delete=models.DO_NOTHING)
+	status = models.TextField(choices=TicketStatus)
+	ticket_type = models.ForeignKey(TicketType, on_delete=models.DO_NOTHING)
