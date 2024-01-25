6ae3bccc79f49d55d168dbceba15ab261bfbd777
Leo Trolin <leo.trolin@gmail.com>
2024-01-25 18:19:34 +0100

    Add staff user

--- backend/bittan/scripts/create_mockdata.py
+++ backend/bittan/scripts/create_mockdata.py
@@ -42,10 +42,3 @@ ticket1 = Ticket.objects.create(
             ticket_type = standardbiljett
         )
 
-NOW = datetime.datetime.now()
-
-standardbiljett = TicketType.objects.create(price=199.99, title="Standardbiljett", description="En vanlig biljett.")
-studentbiljett = TicketType.objects.create(price=99.50, title="Studentbiljett", description="En billigare biljett.")
-
-chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365))
-chapter_event1.ticket_types.add(standardbiljett, studentbiljett)
