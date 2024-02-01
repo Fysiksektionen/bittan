e61113c0c24bba9cde79bc352ccef69c4e4c77f9
Gabriel <gabriel.m.calota@gmail.com>
2024-02-01 19:57:16 +0100

    Add ticket and payment in mockdata.

--- backend/bittan/scripts/create_mockdata.py
+++ backend/bittan/scripts/create_mockdata.py
@@ -8,7 +8,9 @@ import django
 django.setup()
 
 from django.contrib.auth.models import User
-from bittan.models import TicketType, ChapterEvent
+from bittan.models import TicketType, ChapterEvent, ticket_type, Payment, Ticket
+from bittan.models.payment import PaymentStatus
+from bittan.models.ticket import TicketStatus
 import datetime
 
 User.objects.create_superuser("admin", None, "admin")
@@ -21,3 +23,21 @@ studentbiljett = TicketType.objects.create(price=99.50, title="Studentbiljett", 
 
 chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365))
 chapter_event1.ticket_types.add(standardbiljett, studentbiljett)
+
+payment1 = Payment.objects.create(
+            time_created = NOW,
+            swish_id = "Hej",
+            status = PaymentStatus.ALIVE,
+            telephone_number = "123-456 78 90",
+            email = "mail@mail.com",
+            total_price = 199.99,
+            sent_email = True
+        )
+
+ticket1 = Ticket.objects.create(
+            external_id = "1234",
+            time_created = NOW,
+            payment = payment1,
+            status = TicketStatus.PAID,
+            ticket_type = standardbiljett
+        )
