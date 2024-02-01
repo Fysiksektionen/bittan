d3606249c3f107fe41b276f5fc20bc82d029206e
Leo Trolin <leo.trolin@gmail.com>
2024-02-01 19:23:52 +0100

    Create first test file

---
+++ backend/bittan/bittan/tests/test_first.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from bittan.models import TicketType
+
+class FirstTestCase(TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_first(self):
+		print("I am running some cool tests.")
