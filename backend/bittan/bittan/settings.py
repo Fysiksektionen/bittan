72eb882faa290b9d4c0e5fb23b1d4bfd2517546c
Gabriel <gabriel.m.calota@gmail.com>
2024-03-01 22:34:19 +0100

    Enable session cookies.

--- backend/bittan/bittan/settings.py
+++ backend/bittan/bittan/settings.py
@@ -110,6 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
