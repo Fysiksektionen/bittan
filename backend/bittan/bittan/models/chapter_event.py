a120c5f40198456a30529a6d1a5980e6109dadd7
Gabriel <gabriel.m.calota@gmail.com>
2024-04-18 21:05:20 +0200

    Move reservation duration from ticket_type to chapter_event.

--- backend/bittan/bittan/models/chapter_event.py
+++ backend/bittan/bittan/models/chapter_event.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class ChapterEvent(models.Model):
 	title = models.TextField()
@@ -6,3 +7,4 @@ class ChapterEvent(models.Model):
 	max_tickets = models.IntegerField()
 	sales_stop_at = models.DateTimeField()
 	ticket_types = models.ManyToManyField('TicketType')
+	reservation_duration = models.DurationField(default=timezone.timedelta(hours=1))
