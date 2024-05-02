41fa75e82e4dab0f22524b43d2c664f1275a22db
Gabriel <gabriel.m.calota@gmail.com>
2024-05-02 18:30:05 +0200

    Fix accidental missed conflict.

--- backend/bittan/bittan/models/ticket_type.py
+++ backend/bittan/bittan/models/ticket_type.py
@@ -5,8 +5,4 @@ class TicketType(models.Model):
 	price = MoneyField(max_digits=19, decimal_places=2, default_currency='SEK')
 	title = models.TextField()
 	description = models.TextField()
-<<<<<<< HEAD
-	reservation_duration = models.DurationField(default=datetime.timedelta(hours=1))
 	is_visible = models.BooleanField(default=True)
-=======
->>>>>>> main
