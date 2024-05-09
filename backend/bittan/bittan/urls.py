696df5782516ec8111dcd864729562f87077f949
Leo Trolin <leo.trolin@gmail.com>
2024-05-09 22:30:24 +0200

    Add url get_chapterevents

--- backend/bittan/bittan/urls.py
+++ backend/bittan/bittan/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views.views import get_chapterevents
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('get_chapterevents/', get_chapterevents)
 ]
