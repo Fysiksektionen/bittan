b810538fd8c8145baee46f813167d858417bda71
Gabriel <gabriel.m.calota@gmail.com>
2024-05-09 21:40:42 +0200

    Add alive_ticket_count property.

--- backend/bittan/bittan/models/chapter_event.py
+++ backend/bittan/bittan/models/chapter_event.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+from bittan.models.payment import PaymentStatus
+
 class ChapterEvent(models.Model):
 	title = models.TextField()
 	description = models.TextField()
@@ -10,19 +12,13 @@ class ChapterEvent(models.Model):
 	reservation_duration = models.DurationField(default=timezone.timedelta(hours=1))
 
 	@property
-	def ticket_count(self) -> int:
+	def alive_ticket_count(self) -> int:
 		"""
 		Returns the total count of alive tickets for the chapter event. 
 		"""
-		return sum(x.ticket_set.filter(status="ALIVE").count() for x in self.ticket_types.all())
-
-	@property
-	def total_price(self) -> float:
-		"""
-		Returns the total price of all paid (non admin-generated) ticets. 
-		"""
-		return sum(x.price * x.ticket_set.filter(status="PAID").count() for x in self.ticket_types.all())
-	
-	def total_price_by_ticket_type(self, ticket_type_id) -> float:
-		return sum(x.price * x.ticket_set.filter(status="PAID").count() for x in self.ticket_types.filter(id=ticket_type_id))
-	
+		ticket_types = self.ticket_types.all()
+		
+		count = 0
+		for ticket_type in ticket_types:
+			count += ticket_type.ticket_set.filter(payment__status__in=[PaymentStatus.PAID, PaymentStatus.RESERVED]).count()				
+		return count
