8d78e5f0c3c9f6a915228a36564939ff48967858
Leo Trolin <leo.trolin@gmail.com>
2024-05-09 17:22:35 +0200

    Create router

--- frontend/bittan/src/index.tsx
+++ frontend/bittan/src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
+import Root from "./routes/root";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ frontend/bittan/src/routes/root.tsx
@@ -0,0 +1,7 @@
+import HomeScreen from './../screens/home';
+
+export default function Root() {
+	return (
+		<HomeScreen/>
+	)
+}
