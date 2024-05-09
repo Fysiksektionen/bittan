911efb673110f5d0249c90756af3d6c6022c1b76
Leo Trolin <leo.trolin@gmail.com>
2024-05-09 16:56:51 +0200

    Add test button

--- frontend/bittan/src/screens/home.tsx
+++ frontend/bittan/src/screens/home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+function testOnClick() {
+	console.log("Click!")
+}
+
 export default function HomeScreen() {
   return (
     <div>
       Hello this is my homescreen.
+	  <button onClick={testOnClick}>Click here to go to other screen.</button>
     </div>
   );
 }
