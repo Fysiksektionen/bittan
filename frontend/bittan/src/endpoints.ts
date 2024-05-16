e2d36c854be5ab167847bf14371a6d5a045faf70
Leo Trolin <leo.trolin@gmail.com>
2024-05-16 21:45:22 +0200

    Remove debug print

--- frontend/bittan/src/endpoints.ts
+++ frontend/bittan/src/endpoints.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { ChapterEvent } from "./types";
   
   export async function getChapterEvents(): Promise<ChapterEvent[]> {
-	console.log("Gonna ask for chapter events")
 	const response = await axios.get<ChapterEvent[]>('http://localhost:8000/get_chapterevents/');
 	return response.data;
   }
