6d8f90aac34bdad722966f0d107cd4c7e9a5bbb1
Leo Trolin <leo.trolin@gmail.com>
2024-05-16 21:31:08 +0200

    Add event_at field

--- backend/bittan/scripts/create_mockdata.py
+++ backend/bittan/scripts/create_mockdata.py
@@ -19,5 +19,9 @@ NOW = datetime.datetime.now()
 standardbiljett = TicketType.objects.create(price=199.99, title="Standardbiljett", description="En vanlig biljett.")
 studentbiljett = TicketType.objects.create(price=99.50, title="Studentbiljett", description="En billigare biljett.")
 
-chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365))
+chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=365))
 chapter_event1.ticket_types.add(standardbiljett, studentbiljett)
+
+
+chapter_event2 = ChapterEvent.objects.create(title="Fysikalen Dag 2", description="Andra dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=366))
+chapter_event2.ticket_types.add(standardbiljett, studentbiljett)
