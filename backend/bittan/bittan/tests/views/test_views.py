e65977998dc926f1391a84e117a2d697b34f4d6c
Leo Trolin <leo.trolin@gmail.com>
2024-05-16 22:14:31 +0200

    Compare with real id in test_filled GetChaptereventsTest

--- backend/bittan/bittan/tests/views/test_views.py
+++ backend/bittan/bittan/tests/views/test_views.py
@@ -18,8 +18,8 @@ class GetChaptereventsTest(TestCase):
 	
 	def test_filled(self):
 		now = timezone.now()
-		ChapterEvent.objects.create(title="Title1", description="Description1", max_tickets=10, sales_stop_at=now+datetime.timedelta(days=365), event_at=now+datetime.timedelta(days=2))
-		ChapterEvent.objects.create(title="Title2", description="Description2", max_tickets=10, sales_stop_at=now+datetime.timedelta(days=365), event_at=now+datetime.timedelta(days=1))
+		ce1_database = ChapterEvent.objects.create(title="Title1", description="Description1", max_tickets=10, sales_stop_at=now+datetime.timedelta(days=365), event_at=now+datetime.timedelta(days=2))
+		ce2_database = ChapterEvent.objects.create(title="Title2", description="Description2", max_tickets=10, sales_stop_at=now+datetime.timedelta(days=365), event_at=now+datetime.timedelta(days=1))
 		c = Client()
 		response = c.get("/get_chapterevents/")
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -27,13 +27,13 @@ class GetChaptereventsTest(TestCase):
 		self.assertEqual(len(data), 2)
 		
 		ce1 = data[0]
-		self.assertEqual(type(ce1['id']), int)
+		self.assertEqual(ce1['id'], ce2_database.id)
 		self.assertEqual(ce1['title'], "Title2")
 		self.assertEqual(ce1['description'], "Description2")
 		self.assertEqual(type(ce1['event_at']), str) # We don't bother checking the exact content because it contains the datetime up to milliseconds
 
 		ce2 = data[1]
-		self.assertEqual(type(ce2['id']), int)
+		self.assertEqual(ce2['id'], ce1_database.id)
 		self.assertEqual(ce2['title'], "Title1")
 		self.assertEqual(ce2['description'], "Description1")
 		self.assertEqual(type(ce2['event_at']), str) # We don't bother checking the exact content because it contains the datetime up to milliseconds
