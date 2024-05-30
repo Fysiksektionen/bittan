81ceaef232356fe677598a50edced60fdeb7f72c
Leo Trolin <leo.trolin@gmail.com>
2024-05-30 22:52:32 +0200

    Add endpoint getChapterEventById

--- frontend/bittan/src/endpoints.ts
+++ frontend/bittan/src/endpoints.ts
@@ -5,3 +5,9 @@ export async function getChapterEvents(): Promise<ChapterEvent[]> {
 	const response = await axios.get<any[]>('http://localhost:8000/get_chapterevents/');
 	return response.data.map(ce => {return {...ce, event_at: new Date(ce.event_at)}});
 }
+
+export async function getChapterEventById(chapterEventId: number): Promise<ChapterEvent> {
+	const response = await axios.get<any>('http://localhost:8000/get_chapterevent_by_id/', {params: {"id": chapterEventId}});
+	const ce = response.data;
+	return {...ce, event_at: new Date(ce.event_at)};
+}
