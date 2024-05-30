ac29ae92ac810ef2592973ab9aa6260faedc174b
Leo Trolin <leo.trolin@gmail.com>
2024-05-30 20:12:52 +0200

    Fix indentation

--- frontend/bittan/src/endpoints.ts
+++ frontend/bittan/src/endpoints.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ChapterEvent } from "./types";
   
-  export async function getChapterEvents(): Promise<ChapterEvent[]> {
+export async function getChapterEvents(): Promise<ChapterEvent[]> {
 	const response = await axios.get<any[]>('http://localhost:8000/get_chapterevents/');
 	return response.data.map(ce => {return {...ce, event_at: new Date(ce.event_at)}});
 }
