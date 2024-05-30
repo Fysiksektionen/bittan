c0b73432d329de04a50660d7edd88e6850ca689f
Leo Trolin <leo.trolin@gmail.com>
2024-05-30 20:04:59 +0200

    Implement useState for chapterEvent

--- frontend/bittan/src/routes/chapterEventPage.tsx
+++ frontend/bittan/src/routes/chapterEventPage.tsx
@@ -1,10 +1,18 @@
 import { useParams } from 'react-router-dom';
-export default function ChapterEventPage() {
-	var { chapterEventId } = useParams();
+import { useLocation } from 'react-router-dom'
+import { ChapterEvent } from '../types';
+import { useEffect, useState } from 'react';
 
+export default function ChapterEventPage() {
+	const { chapterEventId } = useParams();
+	const location = useLocation();
+	
+	const [chapterEvent, setChapterEvent] = useState<null|ChapterEvent>(location.state === null ? null : location.state.chapterEvent);
+	
 	return (
 		<div>
       		Welcome to the Chapter Event page for event id {chapterEventId}.
+			{chapterEvent === null ? `We have null` : `We have CE`}
 	  	</div>
 	)
 }
