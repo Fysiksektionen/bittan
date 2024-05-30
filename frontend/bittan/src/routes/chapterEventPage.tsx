b66f6d62b556d854aec2d427ce989f4937d24d5c
Leo Trolin <leo.trolin@gmail.com>
2024-05-30 22:52:55 +0200

    Fetch chapterevent on load

--- frontend/bittan/src/routes/chapterEventPage.tsx
+++ frontend/bittan/src/routes/chapterEventPage.tsx
@@ -2,13 +2,35 @@ import { useParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom'
 import { ChapterEvent } from '../types';
 import { useEffect, useState } from 'react';
+import { getChapterEventById } from '../endpoints';
+import { AxiosError } from "axios";
 
 export default function ChapterEventPage() {
-	const { chapterEventId } = useParams();
 	const location = useLocation();
-	
+	const [chapterEventId, setChapterEventId] = useState(useParams().chapterEventId as unknown as number);
 	const [chapterEvent, setChapterEvent] = useState<null|ChapterEvent>(location.state === null ? null : location.state.chapterEvent);
 	
+	useEffect(() => {
+		if (chapterEvent !== null) {
+			return;
+		}
+		const loadChapterEvent = async () => {
+			var recievedChapterEvent: null|ChapterEvent = null;
+			try {
+				recievedChapterEvent = await getChapterEventById(chapterEventId);
+			} catch (err) {
+				if (err instanceof AxiosError) {
+					// pass
+				} else {
+					throw err;
+				}
+				return;
+			}
+			setChapterEvent(recievedChapterEvent)
+		}
+		loadChapterEvent();
+	}, [chapterEventId, chapterEvent]);
+
 	return (
 		<div>
       		Welcome to the Chapter Event page for event id {chapterEventId}.
