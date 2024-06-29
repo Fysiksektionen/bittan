dfdaf74f858397e439061c90102df275b6fd1e21
Gabriel <gabriel.m.calota@gmail.com>
2024-06-29 10:25:53 +0200

    Changes to responses to make reflect reality better.

--- backend/bittan/bittan/views/views.py
+++ backend/bittan/bittan/views/views.py
@@ -25,14 +25,14 @@ def reserve_ticket(request: Request) -> Response:
     response_data: dict = request.data
     if min(x["count"] for x in response_data["tickets"]) < 1: 
         return Response(
-            "NegativeTikets",
+            "NegativeTickets",
             status=status.HTTP_403_FORBIDDEN
         )
     reservation_count: int = sum(x["count"] for x in response_data["tickets"])
     chapter_event: ChapterEvent = ChapterEvent.objects.get(id=response_data["chapter_event"])
     if reservation_count > chapter_event.max_tickets - chapter_event.alive_ticket_count:
         return Response(
-            "outOfTickets", 
+            "OutOfTickets", 
             status=status.HTTP_403_FORBIDDEN
         )
     payment = Payment.objects.create(
@@ -61,13 +61,17 @@ def reserve_ticket(request: Request) -> Response:
                 logging.critical("Failed to generate a ticket external id. This should never happen.")
                 return Response(status=500) # Returns status internal server error. 
     request.session["reserved_payment"] = payment.pk
-    return Response(status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
 def start_payment(request):
+    response_data: dict = request.data
     payment_id = request.session["reserved_payment"]
     payment = Payment.objects.get(pk=payment_id)
+
+    # TODO Verify that the payment is not already started. IMPORTANT since someone might be able 
+    # to pay twice for the same tickets otherwise. 
 
     tickets = payment.ticket_set
     chapter_event = tickets.first().ticket_type.chapterevent_set.first()
@@ -78,23 +82,27 @@ def start_payment(request):
         if tickets.count() > chapter_event.max_tickets - chapter_event.alive_ticket_count:
             # Nej --> Informera den stackars kunden om att den är alldeles för långsam.
             # Skriv något vettigt i responsen så att klient vet att biljetterna är slut. 
-            return Response()
+            return Response(
+                "SessionExpired", 
+                status=status.HTTP_403_FORBIDDEN
+            )
         # Ja --> Skapa biljetter (Lås?), uppdatera session och forsätt
         payment.expires_at = timezone.now() + chapter_event.reservation_duration
     # Ja --> Starta payment biljetterns och forstätt
     payment.payment_started = True
-    payment.save(update_fields=["payment_started"])
+    # TODO Verifiera att mailaddressen är en riktig mailaddress.
+    payment.email = response_data["email_address"]
+    payment.save()
 
     # Hämta/beräkna den datan som Swish behöver (belopp, etc)
     total_price = tickets.aggregate(Sum("ticket_type__price"))["ticket_type__price__sum"]
     
-    # TODO eventuell felhantering här. 
+    # TODO eventuell felhantering inför snack med swish här. Även felhantering efter.
     # Interagera med swish, skicka belopp och swish message. Få tillbaka swish_payment_request
     swish = Swish.get_instance() # Detta hämtar swish instansen som är global över hela bittan. I den här kan saker anropas. 
     
     # Används swish-instansen för att skapa ett payment. metod create_swish_payment(self, amount: int, message="") 
     payment_request: SwishPaymentRequest = swish.create_swish_payment(total_price, chapter_event.swish_message)
 
-    # Fråga efter token för swish_id
-
+    # Fråga efter token för swish_id och skicka tillbaka. 
     return Response(payment_request.token)
