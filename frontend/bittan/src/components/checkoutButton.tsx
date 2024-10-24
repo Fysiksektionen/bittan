1aaac83564cc4188a2eafc2e25da0a3edeec6b4b
edvarer <edvarer@kth.se>
2024-10-24 19:09:48 +0200

    boilerplate

---
+++ frontend/bittan/src/components/checkoutButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './checkoutButton.css'
+
+const CheckoutButton: React.FC = () => {
+  return (
+    <button className="checkout-button">Till Betalning</button>
+  );
+};
+
+export default CheckoutButton;
--- frontend/bittan/src/components/header.tsx
+++ frontend/bittan/src/components/header.tsx
@@ -1,10 +1,17 @@
+import React from 'react';
+import './header.css'
 
-
-export default function PageHeader() {
-
-    return (
-        <header>
-            Huvuder
-        </header>
-    )
+interface HeaderProps {
+  title: string;
 }
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <header className="header">
+      <div className="circle"></div>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+export default Header;
