8bf1ea7bae198ea0ce816ba985d7e06a7aa59fde
Gabriel <gabriel.m.calota@gmail.com>
2024-10-31 19:18:12 +0100

    Add ticket validation view.

--- backend/bittan/bittan/views/views.py
+++ backend/bittan/bittan/views/views.py
@@ -1,8 +1,8 @@
-import json
-
 from bittan.models.payment import PaymentStatus
 
 from ..models import ChapterEvent, Ticket, TicketType, Payment
+
+from django.db import models
 
 from rest_framework import status
 from rest_framework.response import Response
@@ -23,3 +23,38 @@ def get_chapterevents(request: Request) -> Response:
     chapterevents = ChapterEvent.objects.filter(sales_stop_at__gt=now).order_by("event_at")
     s = ChapterEventSerializer(chapterevents, many=True)
     return Response(s.data)
+
+class ValidateValidateTicketSerializer(serializers.Serializer):
+    external_id = serializers.CharField()
+
+@api_view(['PUT'])
+def validate_ticket(request: Request) -> Response:
+
+    valid_ser = ValidateValidateTicketSerializer(data=request.data)
+    if valid_ser.is_valid():
+        response_data = valid_ser.validated_data
+    else:
+        return Response(
+                "InvalidRequestData",
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    try:
+        ticket = Ticket.objects.get(external_id=response_data["external_id"])
+    except Ticket.DoesNotExist:
+        return Response({"times_used": -1, "status": "Ticket does not exist"})
+
+    times_used = ticket.times_used
+    ticket.times_used += 1 
+    ticket.save()
+    
+
+    return Response({"times_used": times_used, "status": ticket.payment.status})
+    # Gets a ticket by its (public) id and gets how many times it has 
+    # been used. Increases its usage count. Returns how many times the ticket has been used
+    # excluded from the current time. 
+
+
+
+    
+    
