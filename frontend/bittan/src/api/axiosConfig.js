f7924e0029c2537b16f6119428bdbf0cfbbb803b
Gustaf Haglund <github@ghagl.se>
2024-11-28 20:17:29 +0100

    frontend ska prata med backend! ✊

--- frontend/bittan/src/api/axiosConfig.js
+++ frontend/bittan/src/api/axiosConfig.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost/backend',
-  withCredentials: true,
+	baseURL: "http://localhost:8000/",
+	withCredentials: true,
 });
 
 export default axiosInstance;
