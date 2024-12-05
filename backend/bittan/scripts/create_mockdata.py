8e37afd62708640ed747b38910bf67542f4ca530
Gabriel <gabriel.m.calota@gmail.com>
2024-12-05 19:46:30 +0100

    More cleanup.

--- backend/bittan/scripts/create_mockdata.py
+++ backend/bittan/scripts/create_mockdata.py
@@ -20,10 +20,10 @@ NOW = datetime.datetime.now()
 standardbiljett = TicketType.objects.create(price=200, title="Standardbiljett", description="En vanlig biljett.")
 studentbiljett = TicketType.objects.create(price=100, title="Studentbiljett", description="En billigare biljett.")
 
-chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=365))
+chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=366))
 chapter_event1.ticket_types.add(standardbiljett, studentbiljett)
 
-chapter_event2 = ChapterEvent.objects.create(title="Fysikalen Dag 2", description="Andra dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365), swish_message="Hej på dig du. ")
+chapter_event2 = ChapterEvent.objects.create(title="Fysikalen Dag 2", description="Andra dagen av Fysikalen.", max_tickets=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=366), swish_message="Hej på dig du. ")
 
 
 payment1 = Payment.objects.create(
