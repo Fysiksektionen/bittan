2ae7448c17c9da8a831815ff5016cb9206874d0a
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-02-20 21:18:51 +0100

    Send mail on sucessful payment

--- backend/bittan/bittan/services/ticket_processing/payment_request_callback_handler.py
+++ backend/bittan/bittan/services/ticket_processing/payment_request_callback_handler.py
@@ -2,6 +2,8 @@ from bittan.services.swish.swish import payment_signal
 from django.dispatch import receiver
 from bittan.models.payment import Payment, PaymentStatus
 
+from bittan.mail import mail_payment
+
 
 def payment_request_callback_handler(sender, **kwargs):
 	payment_request = kwargs["payment_request"]
@@ -9,6 +11,9 @@ def payment_request_callback_handler(sender, **kwargs):
 	payment = Payment.objects.get(swish_id=payment_request.id) 
 	if payment_request.is_paid(): 
 		payment.status = PaymentStatus.PAID 
+		# TODO handle errors
+		mail_payment(payment)
+
 
 	if payment_request.is_failed():
 		payment.status = PaymentStatus.FAILED_EXPIRED_RESERVATION
