ce99e9b4c7b49d8163b68abf571410005ca7a837
Gabriel <gabriel.m.calota@gmail.com>
2025-02-20 18:48:59 +0100

    Add payment method field to Payment model.

--- backend/bittan/bittan/models/payment.py
+++ backend/bittan/bittan/models/payment.py
@@ -6,6 +6,10 @@ class PaymentStatus(models.TextChoices):
 	FAILED_EXPIRED_RESERVATION = "FAILED_EXPIRED_RESERVATION"
 	FAILED_ADMIN = "FAILED_ADMIN"
 
+class PaymentMethod(models.TextChoices):
+	SWISH = "SWISH"
+	ADMIN_GENERATED = "ADMIN_GENERATED"
+
 class Payment(models.Model):
 	expires_at = models.DateTimeField()
 	swish_id = models.TextField(null=True, blank=True)
@@ -13,3 +17,4 @@ class Payment(models.Model):
 	email = models.TextField(null=True, blank=True)
 	sent_email = models.BooleanField(default=False)
 	payment_started = models.BooleanField(default=False)
+	payment_method = models.TextField(choices=PaymentMethod, null=True, blank=True)
