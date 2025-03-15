afa439b47e7e294a52fd0872e2de168945e23676
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-03-15 17:52:20 +0100

    Make mock data create an event which suits the bittan patch

--- backend/bittan/scripts/create_mockdata.py
+++ backend/bittan/scripts/create_mockdata.py
@@ -3,8 +3,9 @@
 import os, sys
 
 sys.path.append("..")
-os.environ['DJANGO_SETTINGS_MODULE'] = "bittan.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "bittan.settings"
 import django
+
 django.setup()
 
 from django.contrib.auth.models import User
@@ -17,29 +18,38 @@ User.objects.create_user("staff", None, "staff", is_staff=True)
 
 NOW = datetime.datetime.now()
 
-standardbiljett = TicketType.objects.create(price=200, title="Standardbiljett", description="En vanlig biljett.")
-studentbiljett = TicketType.objects.create(price=100, title="Studentbiljett", description="En billigare biljett.")
+bittanmarke = TicketType.objects.create(
+	price=25,
+	title="Bittanmärke",
+	description="Ett märke väldigt fint märke. Du köper det nu och får en biljett för att hämta ut märket i kons!",
+)
 
-chapter_event1 = ChapterEvent.objects.create(title="Fysikalen Dag 1", description="Första dagen av Fysikalen.", total_seats=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=365))
-chapter_event1.ticket_types.add(standardbiljett, studentbiljett)
+chapter_event1 = ChapterEvent.objects.create(
+	title="Bittanmärke",
+	description="Ett märke väldigt fint märke. Du köper det nu och får en biljett som du kan använda för att hämta ut ditt märke i kons!",
+	total_seats=80,
+	sales_stop_at=NOW + datetime.timedelta(days=365),
+	event_at=NOW + datetime.timedelta(days=365),
+)
+chapter_event1.ticket_types.add(bittanmarke)
 
 
-payment1 = Payment.objects.create(
-            expires_at = NOW + datetime.timedelta(hours=1),
-            swish_id = "Hej",
-            status = PaymentStatus.RESERVED,
-            email = "mail@mail.com",
-            sent_email = False,
-            payment_method = PaymentMethod.SWISH,
-        )
-
-ticket1 = Ticket.objects.create(
-            external_id = "1234",
-            time_created = NOW,
-            payment = payment1,
-            ticket_type = standardbiljett,
-            chapter_event = chapter_event1
-        )
-
-chapter_event2 = ChapterEvent.objects.create(title="Fysikalen Dag 2", description="Andra dagen av Fysikalen.", total_seats=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=366))
-chapter_event2.ticket_types.add(standardbiljett, studentbiljett)
+# payment1 = Payment.objects.create(
+#             expires_at = NOW + datetime.timedelta(hours=1),
+#             swish_id = "Hej",
+#             status = PaymentStatus.RESERVED,
+#             email = "mail@mail.com",
+#             sent_email = False,
+#             payment_method = PaymentMethod.SWISH,
+#         )
+#
+# ticket1 = Ticket.objects.create(
+#             external_id = "1234",
+#             time_created = NOW,
+#             payment = payment1,
+#             ticket_type = standardbiljett,
+#             chapter_event = chapter_event1
+#         )
+#
+# chapter_event2 = ChapterEvent.objects.create(title="Fysikalen Dag 2", description="Andra dagen av Fysikalen.", total_seats=10, sales_stop_at=NOW+datetime.timedelta(days=365), event_at=NOW+datetime.timedelta(days=366))
+# chapter_event2.ticket_types.add(standardbiljett, studentbiljett)
