2c5a07be5661974d68fbc6cfc9c4361bcd5ec766
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-03-15 14:09:22 +0100

    Handle mail errors

--- backend/bittan/bittan/services/ticket_processing/payment_request_callback_handler.py
+++ backend/bittan/bittan/services/ticket_processing/payment_request_callback_handler.py
@@ -1,4 +1,6 @@
-from bittan.services.swish.swish import payment_signal 
+import logging
+from bittan.mail.mail import MailError
+from bittan.services.swish.swish import payment_signal
 from django.dispatch import receiver
 from bittan.models.payment import Payment, PaymentStatus
 
@@ -8,12 +10,14 @@ from bittan.mail import mail_payment
 def payment_request_callback_handler(sender, **kwargs):
 	payment_request = kwargs["payment_request"]
 
-	payment = Payment.objects.get(swish_id=payment_request.id) 
-	if payment_request.is_paid(): 
-		payment.status = PaymentStatus.PAID 
-		# TODO handle errors
+	payment = Payment.objects.get(swish_id=payment_request.id)
+	payment.status = PaymentStatus.PAID
+	try:
 		mail_payment(payment)
-
+	except MailError as e:
+		logging.warning(f'Unable to send mail of payment {payment.pk} due to a mail error: {e}')
+	except Exception as e:
+		logging.warning(f'Unable to send mail of payment {payment.pk}, {e}')
 
 	if payment_request.is_failed():
 		payment.status = PaymentStatus.FAILED_EXPIRED_RESERVATION
