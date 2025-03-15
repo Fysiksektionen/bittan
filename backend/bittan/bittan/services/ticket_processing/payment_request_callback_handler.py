2c819c49cc2e8cb42107f3aebd9c5868df16fcb8
Gabriel <gabriel.m.calota@gmail.com>
2025-03-15 15:49:20 +0100

    Set date_paid and bring back the check for if the payment is paid.

--- backend/bittan/bittan/services/ticket_processing/payment_request_callback_handler.py
+++ backend/bittan/bittan/services/ticket_processing/payment_request_callback_handler.py
@@ -9,18 +9,20 @@ from bittan.mail import mail_payment
 
 def payment_request_callback_handler(sender, **kwargs):
 	payment_request = kwargs["payment_request"]
-
 	payment = Payment.objects.get(swish_id=payment_request.id)
-	payment.status = PaymentStatus.PAID
-	try:
-		mail_payment(payment)
-	except MailError as e:
-		# TODO mail staff?
-		logging.warning(
-			f'Unable to send mail of payment {payment.pk} due to a mail error: {e}'
-		)
-	except Exception as e:
-		logging.warning(f'Unable to send mail of payment {payment.pk}, {e}')
+	
+	if payment_request.is_paid():
+		payment.status = PaymentStatus.PAID
+		payment.time_paid = payment_request.date_paid
+		try:
+			mail_payment(payment)
+		except MailError as e:
+			# TODO mail staff?
+			logging.warning(
+				f'Unable to send mail of payment {payment.pk} due to a mail error: {e}'
+			)
+		except Exception as e:
+			logging.warning(f'Unable to send mail of payment {payment.pk}, {e}')
 
 	if payment_request.is_failed():
 		payment.status = PaymentStatus.FAILED_EXPIRED_RESERVATION
