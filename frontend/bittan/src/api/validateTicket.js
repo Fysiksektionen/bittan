9b25ce20d3f82c969dec124892b12f1eca8d4fa7
edvarer <edvarer@kth.se>
2025-03-26 12:18:49 +0100

    no csrf??

--- frontend/bittan/src/api/validateTicket.js
+++ frontend/bittan/src/api/validateTicket.js
@@ -3,9 +3,14 @@ import axiosInstance from './axiosConfig';
 
 export const validateTicket = async (externalId, password) => {
   try {
+    const csrf = document.querySelector('meta[name="csrf-token"]');
+
     const response = await axiosInstance.put('/validate_ticket/', {
       external_id: externalId,
-      password: password
+      password: password,
+      headers: {
+        'X-CSRF-TOKEN': csrf
+      }
     });
     return response.data; // Returns ticket validation data
   } catch (error) {
