4695528ecdb7ba79cecc6b138d3917293493598d
Gabriel <gabriel.m.calota@gmail.com>
2025-03-27 19:35:58 +0100

    Add imports for the payment linking in ticket admin.

--- backend/bittan/bittan/admin.py
+++ backend/bittan/bittan/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db.models import Sum
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import TicketType, ChapterEvent, Payment, Ticket, SwishPaymentRequestModel
 
