25668d34f51e7805664376deb80ba479e890037f
Gabriel <gabriel.m.calota@gmail.com>
2025-08-23 22:06:26 +0200

    Add choice-classes to the init.py.

--- backend/bittan/bittan/models/__init__.py
+++ backend/bittan/bittan/models/__init__.py
@@ -3,7 +3,7 @@ from .payment import Payment
 from .ticket_type import TicketType
 from .ticket import Ticket
 from .swish_payment_request import SwishPaymentRequestModel
-from .question import Question
-from .question_option import QuestionOption
+from .question import Question, QuestionType
+from .question_option import QuestionOption, FieldOptions
 from .answer import Answer
 from .answer_selected_options import AnswerSelectedOptions
