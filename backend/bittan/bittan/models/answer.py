7768d647eaddcee0f40ccb3179445752c0263447
Jonathan Niklasson Godar <75271757+JonathanGodar@users.noreply.github.com>
2025-10-02 20:42:27 +0200

    Revert "Fkm/new models"

--- backend/bittan/bittan/models/answer.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Answer(models.Model):
-    question = models.ForeignKey("Question", on_delete=models.DO_NOTHING)
-    ticket = models.ForeignKey("Ticket", on_delete=models.DO_NOTHING)
--- backend/bittan/bittan/models/answer_selected_options.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class AnswerSelectedOptions(models.Model):
-    question_option = models.ForeignKey("QuestionOption", on_delete=models.DO_NOTHING)
-    anwser = models.ForeignKey("Answer", on_delete=models.DO_NOTHING)
-    text = models.TextField(default="")
-
