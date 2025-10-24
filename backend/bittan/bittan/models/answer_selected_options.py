49cc64aed333d95729ff30f83af40f5407e462c6
Gabriel <gabriel.m.calota@gmail.com>
2025-10-24 11:45:36 +0200

    Remove FieldOption

--- backend/bittan/bittan/models/answer_selected_options.py
+++ backend/bittan/bittan/models/answer_selected_options.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class AnswerSelectedOptions(models.Model):
     question_option = models.ForeignKey("QuestionOption", on_delete=models.DO_NOTHING)
-    anwser = models.ForeignKey("Answer", on_delete=models.DO_NOTHING)
+    answer = models.ForeignKey("Answer", on_delete=models.DO_NOTHING)
     text = models.TextField(default="")
 
