3b197045ecb012f7097d72ae48aaad5088d4c5b3
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-10-30 20:26:31 +0100

    Add event statistics endpoint

--- backend/bittan/bittan/api/event_admin.py
+++ backend/bittan/bittan/api/event_admin.py
@@ -5,10 +5,30 @@ from rest_framework.decorators import api_view
 from rest_framework import serializers
 
 
+
 def get_event_meta():
     pass
 
 
+def GetEventStatisticsSerializer(serializers.Serializer):
+    chapter_event = serializers.CharField(required = True)
+
+def get_event_statistics(request: Request):
+    request_data: dict
+
+    valid_ser = ReserveTicketRequestSerializer(data=request.data)
+    if valid_ser.is_valid():
+        request_data = valid_ser.event_chapter
+    else:
+        return Response("InvalidRequestData", status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        chapter_event: ChapterEvent = ChapterEvent.objects.get(id=request_data["chapter_event"])
+    except (ObjectDoesNotExist, ValueError):
+        return Response(
+            "EventDoesNotExist",
+            status=status.HTTP_404_NOT_FOUND
+        )
 
 
 def TicketSerializerFilter(serializers.Serializer):
