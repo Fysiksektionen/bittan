bb97b077a05d1435db8a8cbb76d1f0f9ee47cace
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2025-11-27 21:09:54 +0100

    Answer selected option text is now nullable

--- backend/bittan/bittan/models/answer_selected_options.py
+++ backend/bittan/bittan/models/answer_selected_options.py
@@ -1,7 +1,7 @@
 from django.db import models
 
+
 class AnswerSelectedOptions(models.Model):
     question_option = models.ForeignKey("QuestionOption", on_delete=models.DO_NOTHING)
     answer = models.ForeignKey("Answer", on_delete=models.DO_NOTHING)
-    text = models.TextField(default="")
-
+    text = models.TextField(null=True)
